caa263d94cc0610e318f4a5f898de75b1575f614
Anthony-1999 <anthony.mayhuay.r@uni.pe>
2025-06-08 00:01:00 -0300

    cambio de url

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def redirigir():
         f.write(f"{datetime.now()}, {request.remote_addr}, {request.user_agent}\n")
 
     # Cambia esta URL por tu destino real
-    return redirect("https://ejemplo.com", code=302)
+    return redirect("https://linktr.ee/Crac.UAD?utm_source=qr_code", code=302)
