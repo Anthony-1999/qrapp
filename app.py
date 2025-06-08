0622071e73070b6ce1faabcf36da4de1c808ec5b
Anthony-1999 <anthony.mayhuay.r@uni.pe>
2025-06-07 23:41:24 -0300

    primera version

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, redirect
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def redirigir():
+    # Guarda detalles en un archivo de texto
+    with open("logs.txt", "a") as f:
+        f.write(f"{datetime.now()}, {request.remote_addr}, {request.user_agent}\n")
+
+    # Cambia esta URL por tu destino real
+    return redirect("https://ejemplo.com", code=302)
